dc971373d8ceffa25be5bda39a26306ba1cc80a5
talves <tony@3alves.com>
2019-11-10 16:17:46 -0800

    fix: updates

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -4,14 +4,11 @@ const adaScripts = resolveAdaScripts()
 const doctoc = resolveBin('doctoc')
 
 module.exports = {
-  concurrent: false,
-  linters: {
-    'README.md': [`${doctoc} --maxlevel 3 --notitle`, 'git add'],
-    '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
-      isOptedOut('autoformat', null, `${adaScripts} format`),
-      `${adaScripts} lint`,
-      `${adaScripts} test --findRelatedTests`,
-      isOptedOut('autoformat', null, 'git add'),
-    ].filter(Boolean),
-  },
+  'README.md': [`${doctoc} --maxlevel 3 --notitle`, 'git add'],
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
+    isOptedOut('autoformat', null, `${adaScripts} format`),
+    `${adaScripts} lint`,
+    `${adaScripts} test --findRelatedTests`,
+    isOptedOut('autoformat', null, 'git add'),
+  ].filter(Boolean),
 }
